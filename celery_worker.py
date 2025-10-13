0c5f37bbb88825cf3d026c2aaa15354b9db37a60
Ivan <100131490+Mikforce@users.noreply.github.com>
2025-10-13 10:36:31 +0300

    1
    1

--- celery_worker.py
+++ celery_worker.py
@@ -8,17 +8,15 @@ import sys
 # Добавляем корневую директорию проекта в PYTHONPATH.
 # Это позволяет Celery воркеру находить модули вашего приложения (services, api, и т.д.)
 # независимо от того, откуда он был запущен.
-project_root = os.path.dirname(os.path.abspath(__file__))
-if project_root not in sys.path:
-    sys.path.append(project_root)
-    
+PROJECT_ROOT = "/root/projects/crm-backend/crm-backend-py"
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
 # Имя основного модуля теперь 'celery_worker', как и имя файла.
 celery_app = Celery(
     'celery_worker',
     broker=settings.CELERY_BROKER_URL,
     backend=settings.CELERY_RESULT_BACKEND,
-    # Явно указываем, где находятся наши задачи.
-    # Celery будет искать файл tasks/messaging.py
     include=['tasks.messaging']
 )
 
